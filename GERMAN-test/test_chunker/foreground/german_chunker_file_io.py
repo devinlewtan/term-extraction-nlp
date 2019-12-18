6e63b7eb7b9101a17085d0d0afb61870d2f39726
thomasyu98 <47114548+thomasyu98@users.noreply.github.com>
2019-12-18 15:02:57 -0500

    GERM-TERM 07: Updated entire noun chunker successfully. Fully automated. Takes a .list file, loops through every file and applies noun chunker. Then also updates .list file to one where all of the fields within it are .tchunk file names

---
+++ GERMAN-test/test_chunker/foreground/german_chunker_file_io.py
@@ -0,0 +1,18 @@
+import sys
+
+## takes as input a .list file and outputs a new modified list
+## where all of the old .txt files are renamed to .tchunk files
+
+def update_list(old_list):
+    # split the old file name from its tag and make it the new file name with format XXX_new.list
+    # listname_old = sys.argv[1]
+    listname_old = old_list
+    listname_new = listname_old.rsplit('.',1)[0] + "_new.list"
+
+    with open(listname_old, encoding='utf-8') as input_file, open(listname_new,"w", encoding='utf-8') as output_file:
+        for line in input_file:
+            file_name_without_tag = line.rsplit('.', 1)[0]
+            filename_new = file_name_without_tag+".tchunk\n"
+            output_file.write(filename_new)
+if __name__ == "__main__":
+    update_list()
--- GERMAN-test/test_chunker/foreground/run_noun_chunker.py
+++ GERMAN-test/test_chunker/foreground/run_noun_chunker.py
@@ -1,7 +1,8 @@
-import sys, german_noun_chunker
+import sys, german_noun_chunker, german_chunker_file_io
 
 def main():
     list = sys.argv[1]
+    german_chunker_file_io.update_list(list)
     input_file = open(list, encoding='utf-8')
     for line in input_file:
         german_noun_chunker.run_chunker(line)
